74422fa0ee279b4f5ebaba271e9c56d0b9d7fefd
mymy1327 <my.lengoctra@edu.hyria.fi>
2025-11-10 09:55:36 +0200

    update java script code

--- search_function.js
+++ search_function.js
@@ -38,7 +38,7 @@
         }
 
         /**
-         * Add result into <ul> element
+         * Add result into <ul> element and show them in a list
          */
         function renderResults(results) {
 
@@ -67,6 +67,12 @@
                 const p = document.createElement('p');
                 p.textContent = `${result.price}`;
                 div.appendChild(p);
+                const i = document.createElement('i');
+                i.classList.add('bx', 'bx-plus', 'bx-flip-horizontal', 'col-3');
+                li.appendChild(i);
+                i.addEventListener('click', () => {
+                    addToCart(result);
+                });
                 searchResults.appendChild(li);
             });
 
@@ -78,3 +84,35 @@
             searchResults.style.display = 'none';
             searchInput.value = '';
         });
+        // adding products into cart page
+        const selectedProducts = [];
+        const productList = document.getElementById('product-list');
+        function addToCart(product) {
+            selectedProducts.push(product);
+
+            // show products in cart page
+            productList.innerHTML = '';
+            selectedProducts.forEach(item => {
+                const li = document.createElement('li');
+                li.classList.add('container', 'row');
+
+                const img = document.createElement('img');
+                img.classList.add('result_picture', 'col-3');
+                img.src = `${item.picture}`;
+                li.appendChild(img);
+
+                const div = document.createElement('div');
+                div.classList.add('result_name_price', 'col-6');
+                li.appendChild(div);
+
+                const h3 = document.createElement('h3');
+                h3.textContent = `${item.name}`;
+                div.appendChild(h3);
+
+                const pEl = document.createElement('p');
+                pEl.textContent = `${item.price}`;
+                div.appendChild(pEl);
+
+                productList.appendChild(li);
+            });
+        }
